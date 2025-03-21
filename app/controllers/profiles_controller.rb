bef718cb195efe8ebe4b823e4a0b14b6808a3c96
laureline desplanches <laureline.desplanches@gmail.com>
2025-03-21 15:00:34 +0100

    frontend et modification code dashboard

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,22 +9,24 @@ class ProfilesController < ApplicationController
   def show
   end
 
-
   def create
     @profile = Profile.new(profile_params)
     @profile.user = current_user
-    if @profile.save
+    @profile.hobbie = params[:profile][:hobbies].reject { |hobbie| hobbie == "0" }.join(", ")
+
+    if @profile.save!
       redirect_to authenticated_root_path, notice: "Profil créé avec succès !"
     else
       render :new
     end
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
-    if @profile.update(profile_params)
-      redirect_to dashboard_path, notice: "Profil mis à jour !"
+    if @profile.update!(profile_params)
+      redirect_to profile_path, notice: "Profil mis à jour !"
     else
       render :edit
     end
