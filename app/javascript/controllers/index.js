e321002cb7c976d7052329d75d2bbd786c69dfba
Kevin Saison <kevin.saison91@gmail.com>
2025-03-24 17:08:45 +0100

    fixing map

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,8 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import { Application } from "@hotwired/stimulus"
-window.Stimulus = Application.start()
+// import { Application } from "@hotwired/stimulus"
+// window.Stimulus = Application.start()
 
-import MapToggleController from "./map_toggle_controller.js"
-Stimulus.register("map-toggle", MapToggleController)
+// import MapToggleController from "./map_toggle_controller.js"
+// Stimulus.register("map-toggle", MapToggleController)
